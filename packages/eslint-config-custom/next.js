599cdd06d0900545cbdc60eddf8717b0e1e75fe4
Jeremie Drazic <jerem.drazic@gmail.com>
2023-09-10 00:50:54 +0200

    added storybook app & some cleanup

--- packages/eslint-config-custom/next.js
+++ packages/eslint-config-custom/next.js
@@ -1,6 +1,5 @@
 module.exports = {
   extends: ["next/core-web-vitals", "turbo", "./base.js", "plugin:prettier/recommended"],
-  ignorePatterns: ['node_modules', 'dist'],
   settings: {
     react: {
       version: "detect",
